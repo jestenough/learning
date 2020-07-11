400a6495510cd119cc99e9f1cbd66107e1b82cad
rteff <rtefforg@gmail.com>
2020-07-11 22:39:50 +0300

    Ok, first steps

---
+++ Bash/First.sh
@@ -0,0 +1,26 @@
+#! /bin/bash
+
+# wow u a so cool
+usability="-------------------------------------------"
+
+echo $usability
+echo -e "\033[32mIf u have binary code \033[31m u needn't notes\033[0m"
+echo $usability
+
+echo Hey, what time is it?
+date
+echo $usability
+
+echo Home for the current user is: $HOME
+echo $usability
+
+echo $USER have \$1 in pocket
+echo $usability
+
+mydir=$(pwd)
+echo $mydir
+echo $usability
+
+var1=$((5+5))
+echo $var1
+
