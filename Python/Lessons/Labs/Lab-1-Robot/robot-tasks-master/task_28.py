b878fc73af1ef555860ea951048a384b12aa523b
Vyacheslav Dmitriev <rtefforg@gmail.com>
2020-08-04 14:08:55 +0300

    Added result task_28 and task_29

--- Python/Lessons/Labs/Lab-1-Robot/robot-tasks-master/task_28.py
+++ Python/Lessons/Labs/Lab-1-Robot/robot-tasks-master/task_28.py
@@ -5,7 +5,13 @@ from pyrob.api import *
 
 @task
 def task_7_6():
-    pass
+    n = 0
+    while not wall_is_on_the_right():
+        move_right()
+        if cell_is_filled():
+            n+=1
+        if n == 5:
+            break
 
 
 if __name__ == '__main__':
--- Python/Lessons/Labs/Lab-1-Robot/robot-tasks-master/task_29.py
+++ Python/Lessons/Labs/Lab-1-Robot/robot-tasks-master/task_29.py
@@ -5,8 +5,16 @@ from pyrob.api import *
 
 @task
 def task_7_7():
-    pass
-
+    
+    n = 0
+    while not wall_is_on_the_right():
+        move_right()
+        if cell_is_filled():
+            n+=1
+        else:
+            n = 0
+        if n == 3:
+            break
 
 if __name__ == '__main__':
     run_tasks()
