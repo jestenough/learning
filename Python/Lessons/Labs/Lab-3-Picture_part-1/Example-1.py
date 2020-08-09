2db4183864eacbaeb813e47ac31f97d61d41ff03
Vyacheslav Dmitriev <rtefforg@gmail.com>
2020-08-09 16:28:23 +0300

    Added First example (python-lab-3)

---
+++ Python/Lessons/Labs/Lab-3-Picture_part-1/Example-1.py
@@ -0,0 +1,14 @@
+from graph import *
+
+penColor(255,0,255)
+penSize(5)
+brushColor("blue")
+rectangle(100, 100, 300, 200)
+brushColor("yellow")
+polygon([(100,100), (200,50),
+         (300,100), (100,100)])
+penColor("white")
+brushColor("green")
+circle(200, 150, 50)
+
+run()
