3940ade5270ac798bde1b993ebe0a61b5bac4a63
Vyacheslav Dmitriev <rtefforg@gmail.com>
2020-09-05 21:49:43 +0300

    Added the full Lab-2 (C#)

---
+++ Languages/C#/Labs/Lab-2/Task-1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Привет, Славик!");
+        }
+    }
+}
---
+++ Languages/C#/Labs/Lab-2/Task-3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Task_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите значение ребра: ");
+            double x = Convert.ToDouble(Console.ReadLine());
+
+            double V = Math.Pow(x,3);
+            double S = 6 * Math.Pow(x,2);
+            
+            Console.WriteLine($"\nОбъем поверхности куба = {V}\nПлощадь поверхности куба = {S}");
+        }
+    }
+}
