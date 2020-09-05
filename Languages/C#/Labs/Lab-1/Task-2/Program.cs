3973d8898e3a2832191f78334d35213317b911c5
Vyacheslav Dmitriev <rtefforg@gmail.com>
2020-09-05 14:19:21 +0300

    Added the full Lab-1 (C#)

---
+++ Languages/C#/Labs/Lab-1/Task-2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите первое число: ");
+            int x1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите второе число: ");
+            int x2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"Сумма:{x1+x2}, разность:{x1-x2}, произведение:{x1*x2}, частное:{(double)x1/x2}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Languages/C#/Labs/Lab-1/Task-3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Task_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите двухзначное число: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"Сумма чисел:{(number/10) + (number%10)}");
+
+        }
+    }
+}
