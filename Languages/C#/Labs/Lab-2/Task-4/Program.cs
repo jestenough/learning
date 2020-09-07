04c4663463e549d1c2dc428b2b4af2f59c7bfa4b
Vyacheslav Dmitriev <rtefforg@gmail.com>
2020-09-07 16:56:11 +0300

    Added the Task-4 for Lab-2 (C#). Completed full lab

---
+++ Languages/C#/Labs/Lab-2/Task-4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Task_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            int numbers = number % 100;
+            int num_1 = numbers / 10;
+            int num_2 = numbers % 10;
+            
+            Console.WriteLine($"Сумма двух последних чисел - {num_1 + num_2}");
+
+        }
+    }
+}
