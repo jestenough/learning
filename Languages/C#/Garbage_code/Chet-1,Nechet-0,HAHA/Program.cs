4175f0b34a4e0fe505f9d4678347f3472914c7f6
Vyacheslav Dmitriev <rtefforg@gmail.com>
2020-09-07 17:18:53 +0300

    Added a new example code (C#)

---
+++ Languages/C#/Garbage_code/Chet-1,Nechet-0,HAHA/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Chet_1_Nechet_0_HAHA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // НЕЛЬЗЯ ИСПОЛЬЗОВАТЬ ЦИКЛЫ ИЛИ УСЛОВНЫЕ ОПЕРАТОРЫ (РАЗНОГО РОДА)
+            
+            // Вводится число. узнать, если оно чётное - вывести 1, а если нечетное - 0
+
+            
+            // Способ 1
+            Console.Write("> ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            number++;
+            number = number % 2;
+
+
+            // Способ 2
+            Console.Write("> ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            number %= 2 - 1;
+            number = Math.Abs(number);
+
+
+            // Способ 3
+            // Через bool и функцию
+
+            //Console.WriteLine(number);
+        }
+    }
+}
