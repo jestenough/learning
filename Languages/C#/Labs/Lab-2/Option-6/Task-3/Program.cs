2a578e275b1f71598c45d8f1f5ec837427ae362b
Vyacheslav Dmitriev <rtefforg@gmail.com>
2020-09-09 19:31:11 +0300

    Added the full Lab-2 (Option-6) (C#)

---
+++ Languages/C#/Labs/Lab-2/Option-6/Task-3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Task_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите первую сторону треугольника");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите вторую сторону треугольника");
+            double b = Convert.ToDouble(Console.ReadLine());
+
+            double P = 2 * (a + b);
+            double S = a * b;
+            
+            Console.WriteLine("\nПериметр прямоугольника = {0:0.###}, Площадь прямоугольника = {1:0.###}",P,S);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Languages/C#/Labs/Lab-2/Option-6/Task-4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Task_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число a:");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            int a1 = a*a;    // (a^2)                             // 1
+            int a2 = a1*a1;     // (a^4)                          // 2
+            int a3 = a2*a2;    // (a^8)                           // 3
+            int a_28 = a3*a3*a3*a2;    // 8+8+8+4=28 (a^28)       // 4
+            
+            Console.WriteLine("a^28 = {0}",a_28);
+        }
+    }
+}
