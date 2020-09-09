0e95a703c711da34f86b059e7a783adcd2d176c2
Vyacheslav Dmitriev <rtefforg@gmail.com>
2020-09-09 18:45:58 +0300

    Added the full Lab-2 (Option-5) (C#)

---
+++ Languages/C#/Labs/Lab-2/Option-5/Task-3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Task_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите радиус круга");
+            double r = Convert.ToDouble(Console.ReadLine());
+            double pi = Math.PI;
+
+            double P = 2 * pi * r;
+            double S = pi * Math.Pow(r,2);
+            
+            Console.WriteLine("\nДлина круга = {0:0.###}, Площадь круга = {1:0.###}",P,S);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Languages/C#/Labs/Lab-2/Option-5/Task-4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Task_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите 5-значное число");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+
+            // 12345
+
+            int number_last_num = number % 10; // 5
+            int number_last = number / 10;     // 1234
+
+            int end = number_last_num * 10000 + number_last;
+
+            Console.WriteLine("\nЗачеркнул последнюю цифру({0}) и приписал её в начале - {1}",number_last_num,end);
+            Console.ReadKey();
+        }
+    }
+}
