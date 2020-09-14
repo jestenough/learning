58e42b1e86094ebdc629004ac692efcbf564714c
Vyacheslav Dmitriev <rtefforg@gmail.com>
2020-09-14 16:01:01 +0300

    Update Lab-3(Option-5) (C#)...some changes

--- Languages/C#/Labs/Lab-3/Option-5/Task-5/Program.cs
+++ Languages/C#/Labs/Lab-3/Option-5/Task-5/Program.cs
@@ -11,18 +11,11 @@ namespace Task_5
         
             switch (x)
             {
-                case 1: Console.WriteLine("Месяц: Январь. I квартал"); break;
-                case 2: Console.WriteLine("Месяц: Февраль. I квартал"); break;
-                case 3: Console.WriteLine("Месяц: Март. I квартал"); break;
-                case 4: Console.WriteLine("Месяц: Апрель. II квартал"); break;
-                case 5: Console.WriteLine("Месяц: Май. II квартал"); break;
-                case 6: Console.WriteLine("Месяц: Июнь. II квартал"); break;
-                case 7: Console.WriteLine("Месяц: Июль. III квартал"); break;
-                case 8: Console.WriteLine("Месяц: Август. III квартал"); break;
-                case 9: Console.WriteLine("Месяц: Сентябрь. III квартал"); break;
-                case 10: Console.WriteLine("Месяц: Октябрь. IV квартал"); break;
-                case 11: Console.WriteLine("Месяц: Ноябрь. IV квартал"); break;
-                case 12: Console.WriteLine("Месяц: Декабрь. IV квартал"); break;
+                case 1: case 2: case 3: Console.WriteLine("I квартал"); break;
+                case 4: case 5: case 6: Console.WriteLine("II квартал"); break;
+                case 7: case 8: case 9: Console.WriteLine("III квартал"); break;
+                case 10: case 11: case 12: Console.WriteLine("IV квартал"); break;
+                default: Console.WriteLine("Такого месяца нет"); break;
             }
         }
     }
