34090350b42d34b3288488ea36ab0c0c1146838f
Vyacheslav Dmitriev <rtefforg@gmail.com>
2020-09-14 20:20:47 +0300

    Added new simple code

---
+++ Languages/C#/Garbage_code/Example/Max_of_N/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Max_of_N
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите N: ");
+            int N = Convert.ToInt32(Console.ReadLine());
+            int max = Int32.MinValue;
+            int num;
+
+            for (int i = 0; i < N; i++)
+            {
+                Console.Write("> ");
+                num = Convert.ToInt32(Console.ReadLine());
+                if (num > max)  max = num;
+            }
+            Console.WriteLine($"Максимальное число:{max}");
+        }
+    }
+}
