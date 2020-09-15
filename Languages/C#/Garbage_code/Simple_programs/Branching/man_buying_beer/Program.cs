8daa23b6b332cb5fd2bd3580bd8364ecd66f7703
Vyacheslav Dmitriev <rtefforg@gmail.com>
2020-09-15 16:25:28 +0300

    mv folders (Garbage_code - C#) and del shit_code

---
+++ Languages/C#/Garbage_code/Simple_programs/Branching/man_buying_beer/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace man_buying_beer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Привет, какого ты года рождения?\n> ");
+            int year = Convert.ToInt32(Console.ReadLine());
+            Console.Write("А какой месяц рождения?\n> ");
+            int mounth = Convert.ToInt32(Console.ReadLine());
+            Console.Write("А какой день?\n> ");
+            int day = Convert.ToInt32(Console.ReadLine());
+
+
+            if (DateTime.Now.Year - year > 18) Console.WriteLine("Отлично, Вам есть 18");  
+            else if ((DateTime.Now.Year - year == 18) && (DateTime.Now.Month > mounth)) Console.WriteLine("Отлично, Вам есть 18");  
+            else if (DateTime.Now.Year - year == 18 && DateTime.Now.Month == mounth && DateTime.Now.Day > day) Console.WriteLine("Отлично, Вам есть 18");  
+            else Console.WriteLine("Не продам, Вам нет 18-ти");
+  
+
+
+        }
+    }
+}
