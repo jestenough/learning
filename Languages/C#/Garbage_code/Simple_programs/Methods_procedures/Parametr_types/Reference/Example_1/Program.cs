18d2d5e8640ad571b0d0495b9b2ee2993672c779
Vyacheslav Dmitriev <rtefforg@gmail.com>
2020-09-17 21:38:30 +0300

    Added new exmaple (methods) (C#)

---
+++ Languages/C#/Garbage_code/Simple_programs/Methods_procedures/Parametr_types/Reference/Example_1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Example_1
+{
+    class Program
+    {
+        static void Function(ref int a)
+        {
+            a++;
+        }
+        static void Main(string[] args)
+        {
+            int a = 10;
+            Function(ref a);
+            Console.WriteLine(a); // 11
+        }
+    }
+}
