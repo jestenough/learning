4ec15ad36277d4ca600a09ef9e36110f895a736d
Vyacheslav Dmitriev <rtefforg@gmail.com>
2020-09-18 13:52:32 +0300

    Added full tasks (methods)

---
+++ Languages/C#/Garbage_code/Simple_programs/Methods_procedures/Task-4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Task_4
+{
+    class Program
+    {
+        static void converter(double byn, out double usd, out double eur)
+        {
+            usd = byn * 0.3883;
+            eur = byn * 0.3293;
+        }
+        static void Main(string[] args)
+        {
+            double BYN, USD, EUR;
+            Console.Write("BYN = ");
+            BYN = Convert.ToDouble(Console.ReadLine());
+
+            converter(BYN,out USD, out EUR);
+            USD = Math.Round(USD,3);
+            EUR = Math.Round(EUR,3);
+
+            Console.WriteLine($"USD = {USD}\nEUR = {EUR}");
+        
+        }
+    }
+}
