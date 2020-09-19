f8087aa5848ba1a3eec3db150b41daeef1ae4232
Vyacheslav Dmitriev <rtefforg@gmail.com>
2020-09-19 22:51:20 +0300

    Added lab-3 part-2(C#) all tasks (except the last task (task-3))

---
+++ Languages/C#/Labs/Lab-3_part-2/Task-2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Task_2
+{
+    class Program
+    {
+        /*
+        Вводятся числа до тех пор, пока не введен 0. Найти:
+        - среднее арифметическое введенных чисел,
+        - минимальное значение,
+        - количество положительных и отрицательных значений среди введённых.
+        Завершающий 0 не учитывать
+        */
+        static void Main(string[] args)
+        {
+            int number = 1; // введенное число
+            int min = Int32.MaxValue; // минимальное число
+            int x = 0; // кол-во итераций
+            int summ = 0; // сумма чисел для среднего ариф
+            int positive = 0,negative = 0; // положительнеые и отрицательные значения
+
+            Console.WriteLine("Вводите числа, если решите прекратить, то введите 0");
+            while (true)
+            {
+                Console.Write("> ");
+                number = Convert.ToInt32(Console.ReadLine());
+  
+                if (number == 0) break;
+                else if (number > 0) positive+=1;
+                else negative+=1; 
+
+                if (number < min) min = number;
+
+                summ += number;
+                x += 1;
+            }
+
+            if (x == 0)    Console.WriteLine("Вы ничего не ввели");
+            else
+            {
+                Console.WriteLine("Среднее арифметическое введенных чисел: {0:0.00}",summ/(double)x);
+                Console.WriteLine($"Минимальное значение из введенных чисел: {min}");
+                Console.WriteLine($"Количество положительных значений: {positive}, количество отрицательных: {negative}");
+            } 
+        }
+    }
+}
---
+++ Languages/C#/Labs/Lab-3_part-2/Task-3/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Task_3
+{
+    class Program
+    {
+        /*
+        Решить задачу, применив оператор повтора FOR.
+        Найти сумму ряда
+            a) конечного, на n слагаемых
+            b) бесконечного, с точностью eps, которую вводит пользователь. 
+        */
+        static void Main(string[] args)
+        {
+            int n;
+            double x, summa = 0,eps;
+
+            // a) конечного, на n слагаемых
+            Console.Write("Введите количество слагаемых\n> ");
+            n = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите x\n> ");
+            x = Convert.ToDouble(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                summa += Math.Pow((-1),i) * (x / Math.Pow(i,3));
+            }
+
+            Console.WriteLine($"Сумма ряда, на {n} слагаемых = {summa}");
+
+            // b) бесконечного, с точностью eps, которую вводит пользователь
+            
+            /*
+            Console.Write("\nВведите точность EPS\n> ");
+            eps = Convert.ToDouble(Console.ReadLine());
+            
+            summa = 0;
+
+            for (int i = 1; x >= eps; i++)
+            {
+                x = Math.Pow((-1),i) * (x / Math.Pow(i,3));
+                summa += x;
+            }
+
+            Console.WriteLine($"Сумма ряда = {summa}");
+            */
+        }
+    }
+}
