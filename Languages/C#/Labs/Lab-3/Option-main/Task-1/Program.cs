048ae93e71332708aaed6568f7d51917e3d585f1
Vyacheslav Dmitriev <rtefforg@gmail.com>
2020-09-21 14:25:48 +0300

    Added full lab-3 main option (C#)

---
+++ Languages/C#/Labs/Lab-3/Option-main/Task-1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Task_1
+{
+    class Program
+    {
+        /*
+        Дано число. 
+        Определить, положительное оно, отрицательное или равно 0. 
+        */
+        
+        static void Main(string[] args)
+        {
+            Console.Write("Введите целое число: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            if (number > 0)       Console.WriteLine($"Ваше число {number} - положительное");
+            else if (number == 0) Console.WriteLine($"Ваше число {number} = 0");
+            else                  Console.WriteLine($"Ваше число {number} - отрицательное");
+        }
+    }
+}
