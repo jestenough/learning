43be269b1bb16cdd7808d1a21ef373279b55785c
Vyacheslav Dmitriev <rtefforg@gmail.com>
2020-09-23 20:29:35 +0300

    Added full lab-4 (C#)

---
+++ Languages/C#/Labs/Lab-4/Task-3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Task_3
+{
+    class Program
+    {
+        /*
+        Описать функцию RectPS(a, b), 
+        вычисляющую площадь S прямоугольника со сторонами a, b.
+        */
+
+        static double RectPS(double a, double b)
+        {
+            return a * b;
+        }
+        static void Main(string[] args)
+        {
+            double a,b;
+            
+            Console.WriteLine("Введите стороны прямоугольника");
+            Console.Write("a = ");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.Write("b = ");
+            b = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("\nПлощадь прямоугольника = {0:0.###}",RectPS(a,b));
+        }
+    }
+}
---
+++ Languages/C#/Labs/Lab-4/Task-4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Task_4
+{
+    class Program
+    {
+        /*
+        Описать метод TriangleP(a, h), 
+        находящий периметр и площадь равнобедренного треугольника 
+        по его основанию (a)
+        и высоте (h), проведенной к основанию (a и h — вещественные). 
+        */
+
+        static void TrianglePS(double a, double h, out double P, out double S)
+        {
+            P = a + 2 * Math.Sqrt(Math.Pow(a/2.0,2) + Math.Pow(h,2));
+            S = 0.5 * a * h;
+        }
+        static void Main(string[] args)
+        {
+            double b,h;
+            double P,S;            
+
+            Console.Write("Введите основание: ");
+            b = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите высоту: ");
+            h = Convert.ToDouble(Console.ReadLine());
+
+            TrianglePS(b,h,out P, out S);
+            Console.WriteLine("\nПериметр = {0:0.###}, площадь = {1:0.###}",P,S);
+        }
+    }
+}
