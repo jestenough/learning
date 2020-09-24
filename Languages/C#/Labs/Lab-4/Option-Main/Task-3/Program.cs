f9f47387d62064b0120eeea6aa9d24ac9c3306bf
Vyacheslav Dmitriev <rtefforg@gmail.com>
2020-09-24 15:28:34 +0300

    Added full lab-4 (main) (C#)

---
+++ Languages/C#/Labs/Lab-4/Option-Main/Task-3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Task_3
+{
+    class Program
+    {
+        /*
+        Описать функцию CubeV(a), 
+        вычисляющую по стороне a куба его объем 
+        V = a3. 
+        */
+
+        static double CubeV(double a)
+        {
+            return Math.Pow(a,3);
+        }
+        static void Main(string[] args)
+        {
+            double a;
+            
+            Console.WriteLine("Введите значение стороны куба");
+            Console.Write("> ");
+            a = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("\nОбъем куба = {0:0.###}",CubeV(a));
+        }
+    }
+}
---
+++ Languages/C#/Labs/Lab-4/Option-Main/Task-4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Task_4
+{
+    class Program
+    {
+        /*
+        Описать метод DigitCount(K), 
+        находящий количество цифр целого положительного числа K. 
+        */
+
+        static int DigitCount(int K)
+        {
+            int count = 0;
+            if (K == 0) return ++count;
+            for (int i = K; i != 0; i/=10) count++;
+            return count;
+        }
+        static void Main(string[] args)
+        {
+            int K;            
+
+            Console.Write("Введите число\n> ");
+            K = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"\nКол-во цифр в числе - {DigitCount(K)}");
+        }
+    }
+}
