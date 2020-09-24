9fe2aecef5d4ef455913ae7c0893cec47ef97cc1
Vyacheslav Dmitriev <rtefforg@gmail.com>
2020-09-24 15:29:35 +0300

    Added tasks for Lab-4 (option-4-old) (C#)

---
+++ Languages/C#/Labs/Lab-4/Option-4-old/Task-2/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace Task_2
+{
+    class Program
+    {
+        /*
+        Дополнить программу пункта 1 методом, возвращающим корни уравнения, 
+        если они есть, через выходные параметры. 
+        Считать, что если у уравнения 1 корень – это 2 равных корня уравнения. 
+        */
+        
+        static int quantity(int a, int b, int c)
+        {
+            int D;
+
+            D = b*b - 4 * a * c;
+            
+            if (D > 0) return 2;
+            else if (D == 0) return 1;
+            else return 0;
+        }
+
+        static void root(int a, int b, int c, out double x1, out double x2)
+        {
+            int D;
+
+            D = b*b - 4 * a * c;
+            if (D > 0)
+            {
+                x1 = (-b - Math.Sqrt(D)) / (2*a);
+                x2 = (-b + Math.Sqrt(D)) / (2*a);
+            }
+            else
+            {
+                x1 = -b / (2*a);
+                x2 = x1;
+            }
+        }
+        static int Main(string[] args)
+        {
+            int a,b,c;
+            double x1,x2;
+
+            Console.Write("Введите а,b,с\n> ");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("> ");
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.Write("> ");
+            c = Convert.ToInt32(Console.ReadLine());
+
+            if (quantity(a,b,c) == 0)
+            {
+                Console.WriteLine("\nТак как дискриминант меньше нуля, то корней нет");
+                return 0;
+            }
+            else
+            {
+                root(a,b,c,out x1,out x2);
+                Console.WriteLine("\nКорни квадратного уравнения\nx1 = {0:0.###}\nx2 = {1:0.###}",x1,x2);
+            }
+
+            return 0;
+        }
+    }
+}
---
+++ Languages/C#/Labs/Lab-4/Option-4-old/Task-5/Program.cs
@@ -0,0 +1,45 @@
+﻿// using System;
+
+// namespace Task_5
+// {
+//     class Program
+//     {
+//         /*
+//         Описать функцию IsPalindrom(K), возвращающую TRUE, 
+//         если целый параметр K (> 0) является палиндромом, 
+//         и FALSE в противном случае. 
+//         С ее помощью найти количество палиндромов в наборе из 10 целых положительных чисел. 
+//         */
+
+//         static bool IsPalindrom(int K)
+//         {
+//             int size = 0;
+            
+//             for(int i = K; i != 0; i/=10)  size++;
+
+//             Console.WriteLine(size);
+
+//             for (int i = 2; i <= (size+1) / 2; i++)
+//             {
+//                 for (int x = 1; i <= (size+1);)
+//                 {
+//                     x*=10;
+//                     for(int j = 1; i <= (size+1) / 2;)
+//                     {
+//                         j*=10;
+//                         if (K % j / x != K / 10 / Math.Pow(10,size-i))  return false;
+//                     }
+//                 }
+//             }
+//             return true;
+//         }
+//         static void Main(string[] args)
+//         {
+//             int K;
+//             Console.Write("Введите число: ");
+//             K = Convert.ToInt32(Console.ReadLine());
+//             Console.WriteLine(IsPalindrom(K));
+
+//         }
+//     }
+// }
