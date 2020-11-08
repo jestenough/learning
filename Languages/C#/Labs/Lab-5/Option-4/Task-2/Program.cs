d437911b52e76e9ee8bf1e80b4470c531ef86a47
Vyacheslav Dmitriev <rtefforg@gmail.com>
2020-11-08 20:16:14 +0300

    Add: Lab-5 (Option-4) [C#]

---
+++ Languages/C#/Labs/Lab-5/Option-4/Task-2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Task_2
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            Console.Write("N: ");
+            int N = Convert.ToInt32(Console.ReadLine());
+            if (N <= 1) return 0;
+            Console.Write("A: ");
+            int A = Convert.ToInt32(Console.ReadLine());
+            Console.Write("D: ");
+            int D = Convert.ToInt32(Console.ReadLine());
+
+            int[] array = new int[N];
+            int D_pow = 1;
+
+            for (int i = 0; i < N; ++i)
+            {
+                D_pow *= D;
+                array[i] = A * D_pow;
+                Console.Write(array[i] + " ");
+            }
+
+            return 0;
+        }
+    }
+}
