478bad746815e0dd762876764f2bcf302621efea
Vyacheslav Dmitriev <rtefforg@gmail.com>
2020-11-10 09:17:26 +0300

    Correct: Lab-5_part-2 (Task-4) [C#]

--- Languages/C#/Labs/Lab-5_part-2/Option-4/Task-4/Program.cs
+++ Languages/C#/Labs/Lab-5_part-2/Option-4/Task-4/Program.cs
@@ -7,8 +7,19 @@ namespace Task_4
         /*
         Определить ступенчатый массив на n строк. 
         длину каждой строки определить случайным образом (больше 0!).
-        */
 
+        Описать МЕТОД, принимающий в качестве аргумента одномерный массив 
+        и возвращающий среднее значение элементов.
+        
+        С помощью описанного метода вывести номера строк, у 
+        которых AVG > 0 и сами значения AVG этих строк
+        */
+        static double array_AVG(int[] example)
+        {
+            int summ = 0;
+            for (int i = 0; i < example.Length; i++)    summ += example[i];
+            return (summ * 1.0) / example.Length;
+        }
 
         static void Main(string[] args)
         {
@@ -22,7 +33,7 @@ namespace Task_4
                 array[i] = new int[rand.Next(2,10)];
                 for (int j = 0; j < array[i].Length; j++)
                 {
-                    array[i][j] = rand.Next(1, 100); 
+                    array[i][j] = rand.Next(-5, 5); 
                 }
             }
 
@@ -35,6 +46,16 @@ namespace Task_4
                     Console.Write($"{number} \t");
                 }
                 Console.WriteLine();
+            }
+            Console.WriteLine();
+            double x = 0;
+            for (int i = 0; i < n; i++)
+            {
+                //Console.WriteLine($"Среднее значение: {array_AVG(array[i])}");
+                x = array_AVG(array[i]);
+                if (x > 0) Console.WriteLine($"AVG строки №{i+1} = {x}");
+                else  Console.WriteLine($"Среднее значение: {x}");
+
             }
         }
     }
