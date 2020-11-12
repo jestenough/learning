eb7be1c20e09bce54014307cd950a9bf2ef14e3d
Vyacheslav Dmitriev <rtefforg@gmail.com>
2020-11-12 20:19:42 +0300

    Add: Lab 6-7 (Option-4) (C#)

---
+++ Languages/C#/Labs/Lab-7/Option-4/Task-2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+
+namespace Task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите текст\n> ");
+            string text = Console.ReadLine();
+            text = text.Replace("да", "нет", true);
+            Console.WriteLine("\n" + text);
+
+            StringBuilder stringb = new StringBuilder(text);        
+            stringb.Replace("нет", "да");
+            //Console.WriteLine("\n" + stringb);
+            string s = stringb.ToString();
+            Console.WriteLine("\n" + s);
+        }
+    }
+}
