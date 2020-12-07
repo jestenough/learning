0743a6575ae0f7734775575f0942b46075bf7497
Vyacheslav Dmitriev <rtefforg@gmail.com>
2020-12-07 14:46:32 +0300

    Add: Lab-16 [C#]

---
+++ Languages/C#/Labs/Lab-16/Task-1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Task_1
+{
+    class ClassCounter 
+    {
+        public delegate void MethodContainer();
+        public event MethodContainer onCount;
+
+        public void Count()
+        {          
+            for (int i = 1; i <= 100; i++)  
+            {  
+                Console.Write(i + " ");
+                if (i == 100)
+                {
+                    if (onCount != null)
+                    {
+                        onCount();
+                    }
+                }
+            }         
+        }
+    }
+    class Handler_I 
+    {
+        public void Message()
+        {
+            Console.WriteLine("Пора действовать, ведь уже 100!");         
+        }                                                        
+    }
+    class Handler_II
+    {
+        public void Message()
+        {            
+            Console.WriteLine("Точно, уже 100!");        
+        }    
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ClassCounter Counter = new ClassCounter();
+            Handler_I Handler1 = new Handler_I();
+            Handler_II Handler2 = new Handler_II();
+            
+            Counter.onCount += Handler1.Message;
+            Counter.onCount += Handler2.Message;
+            Counter.Count();
+        }
+    }
+}
---
+++ Languages/C#/Labs/Lab-16/Task-2/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Task_2
+{
+    /*
+    Реализовать с использованием классов и механизма событий:  
+    Метод считывает вводимую пользователем строку. Если в строке находятся числа, 
+    вывести сообщение «ОБНАРУЖЕНЫ ЧИСЛА!» (один метод) и найти сумму чисел (не цифр!) в строке (другой метод). 
+    Ввод строк организовать циклом (решите сами, каким образом прерывать цикл). 
+    Сообщение и подсчет суммы реализовать двумя методами, а не одним (подписать их на событие).
+    */
+
+    class Stroka
+    {
+        public delegate void MethodContainer();
+        public event MethodContainer numbers_founded;
+        
+        string text;
+
+        public Stroka (string _text)
+        {
+            text = _text;
+        }
+
+        public void find_numbers()
+        {
+            Regex reg = new Regex(@"(\d)+") ;
+            MatchCollection matches = reg.Matches(text);
+
+            int count = matches.Count;
+            if (count != 0)
+                numbers_founded();           
+        }
+
+        public void Message()
+        {
+            Console.WriteLine("ОБНАРУЖЕНЫ ЧИСЛА!");
+        }
+
+        public void summ_numbers()
+        {
+            int summ = 0;
+            Regex reg = new Regex(@"(\d)+") ;
+            MatchCollection matches = reg.Matches(text);
+            foreach(Match match in matches)
+                summ += Convert.ToInt32(match.Value);
+            Console.WriteLine($"Сумма чисел = {summ}");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string stroka;
+
+            while (true)
+            {
+                Console.Write(">>> ");
+                stroka = Console.ReadLine();
+                if (stroka == "славик") break;
+                else
+                {   
+                    Stroka text = new Stroka(stroka);
+                    text.numbers_founded += text.Message;
+                    text.numbers_founded += text.summ_numbers;
+                    text.find_numbers();
+                    Console.ReadKey();
+                }
+            }
+        }
+        
+    }
+}
