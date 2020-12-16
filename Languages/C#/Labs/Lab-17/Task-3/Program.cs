963dcb64dc7180f3c372df3c951ba1862330565a
Vyacheslav Dmitriev <rtefforg@gmail.com>
2020-12-16 21:34:54 +0300

    Add: Lab-17 [C#]

---
+++ Languages/C#/Labs/Lab-17/Task-3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+
+namespace Task_3
+{
+    class Program
+    {
+        /*
+        Дан текстовый файл. 
+        Отсортировать строки в нем по алфавиту.
+        */
+        
+        static void Main(string[] args)
+        {
+            string path = "text.txt";
+            StreamReader sr = new StreamReader(path);
+            
+            int length = File.ReadAllLines(path).Length;
+            string[] array = new string[length];
+            
+            int j = 0;
+            while (!sr.EndOfStream)
+            {
+                array[j] = sr.ReadLine();
+                j++;
+            }
+            sr.Close();
+
+            Array.Sort(array);
+            StreamWriter sw = new StreamWriter(path);
+            for (int i = 0; i < length; i++)
+                sw.WriteLine(array[i]);
+
+            sw.Close();
+        }
+    }
+}
