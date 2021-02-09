a65249779346d7df761bcec4561f947dad254e01
Vyacheslav Dmitriev <rtefforg@gmail.com>
2021-02-09 10:53:25 +0300

    Update: C# folder
    Add: Lab-20 C#

---
+++ Languages/C#/Labs/Lab-20/Task-1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+
+namespace Task_1
+{
+    class Program
+    {
+        /*
+        Используя коллекцию ArrayList, создайте коллекцию, в которую занесите свои фамилию, имя, группу, возраст (как отдельные элементы).
+        Выведите информацию на экран в столбец.
+        Измените значения элементов коллекции (например, имя), удалите отчество из коллекции. Продемонстрируйте результат.
+        */
+        static void Main(string[] args)
+        {
+
+            ArrayList student_info = new ArrayList();
+
+            Console.Write("Фамилия: ");
+            object surname = Console.ReadLine();
+            Console.Write("Имя: ");
+            object name = Console.ReadLine();
+            Console.Write("Отчество: ");
+            object otchestvo = Console.ReadLine();
+            Console.Write("Группа: ");
+            object group = Console.ReadLine();
+            Console.Write("Возраст: ");
+            object age = Convert.ToInt32(Console.ReadLine());
+
+            student_info.Add(surname);
+            student_info.Add(name);
+            student_info.Add(otchestvo);
+            student_info.Add(group);
+            student_info.Add(age);
+
+            int index = student_info.IndexOf(name);
+            student_info[index] = "Ерис";
+            student_info.Remove(otchestvo);
+
+            Console.WriteLine();
+
+            foreach (object item in student_info)
+                Console.WriteLine(item);
+
+
+
+
+
+        }
+    }
+}
---
+++ Languages/C#/Labs/Lab-20/Task-2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task_2
+{
+    class Program
+    {
+        /*
+        Используя коллекцию List<T>, создайте 2 коллекции.
+        В первую занесите целочисленный массив размера N, во вторую – строковый массив такого же размера.
+        Найдите и выведите максимальное число из коллекции чисел и самую длинную строку из коллекции строк.
+        */
+        static void Main(string[] args)
+        {
+            int[] mass = new int[4] {1, 2, 3, 4};
+            string[] mass_2 = new string[4] {"1", "222222222", "333", "4444"};
+
+            List<int> number_1 = new List<int>();
+            number_1.AddRange(mass);
+            List<string> number_2 = new List<string>();
+            number_2.AddRange(mass_2);
+
+
+            int max = Int32.MinValue;
+            foreach (int i in number_1)
+                if (i > max)
+                    max = i;
+
+
+
+            int max2 = Int32.MinValue;
+            
+            foreach (string i in number_2)
+                if (i.Length > max2)
+                    max2 = i.Length;
+
+            Console.WriteLine(max);
+            foreach (string i in number_2)
+                if (i.Length == max2)
+                    Console.WriteLine(i);
+
+
+        }
+    }
+}
