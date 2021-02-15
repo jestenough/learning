82891ca484316b13e1f38bf4eb4acb9869b8d11e
Vyacheslav Dmitriev <rtefforg@gmail.com>
2021-02-15 14:51:25 +0300

    Update: C# folder
    Add: Lab-21

---
+++ Languages/C#/Labs/Lab-21/Task-1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Xml;
+
+namespace Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XmlDocument xml_Doc = new XmlDocument();
+            xml_Doc.Load("test.xml");
+            XmlElement xml_Root = xml_Doc.DocumentElement;
+            // обход всех узлов в корневом элементе
+            foreach (XmlNode xnode in xml_Root)
+            {
+                XmlNode main_attr = xnode.Attributes.GetNamedItem("name");
+                Console.WriteLine(main_attr.Value);
+
+                foreach (XmlNode childnode in xnode.ChildNodes)
+                {
+                    if (childnode.Name == "resolution")
+                        Console.WriteLine($"Разрешение: {childnode.InnerText}");
+
+                    if (childnode.Name == "price")
+                        Console.WriteLine($"Цена: {childnode.InnerText}");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
