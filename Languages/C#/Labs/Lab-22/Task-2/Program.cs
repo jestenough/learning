4e9e9dc25233de589068d8cea5895a0aec475880
Vyacheslav Dmitriev <rtefforg@gmail.com>
2021-02-19 09:15:00 +0300

    Update: C# folder
    Add: Lab-22

---
+++ Languages/C#/Labs/Lab-22/Task-2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Xml.Linq;
+using System.Collections.Generic;
+
+namespace Task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            XDocument xdoc = XDocument.Load("new_file.xml");
+            XElement root = xdoc.Element("cameras");
+
+            foreach (XElement phoneElement in xdoc.Element("cameras").Elements("Camera"))
+            {
+                XAttribute name = phoneElement.Attribute("name");
+                XElement resolution = phoneElement.Element("resolution");
+                XElement price = phoneElement.Element("price");
+
+                Console.WriteLine($"Имя: {name.Value}");
+                Console.WriteLine($"Разрешение: {resolution.Value}");
+                Console.WriteLine($"Цена: {price.Value}");
+
+                Console.WriteLine();
+            }
+
+            root.Add(new XElement("Camera",
+                new XAttribute("name", "new_test"),
+                new XElement("resolution", "1111111"),
+                new XElement("price", "19500")));
+            xdoc.Save("new_file1.xml");
+
+        }
+    }
+}
