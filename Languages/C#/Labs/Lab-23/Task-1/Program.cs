b4d6a62613765fc8176f918eb486f2cf2a84db32
Vyacheslav Dmitriev <rtefforg@gmail.com>
2021-02-26 09:11:27 +0300

    Update: C# folder
    Add: Lab-23

---
+++ Languages/C#/Labs/Lab-23/Task-1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading;
+
+namespace Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread t = Thread.CurrentThread;
+
+            Console.WriteLine($"Имя потока: {t.Name}");
+            Console.WriteLine($"Приоритет потока: {t.Priority}");
+            Console.WriteLine($"Статус потока: {t.ThreadState}");
+
+            t.Name = "Метод СЛавика";
+
+            Console.WriteLine($"Имя потока: {t.Name}");
+            Console.WriteLine($"Приоритет потока: {t.Priority}");
+            Console.WriteLine($"Статус потока: {t.ThreadState}");
+
+        }
+    }
+}
---
+++ Languages/C#/Labs/Lab-23/Task-2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading;
+
+namespace Task_2
+{
+    class Program
+    {
+        public static void Count()
+        {
+            for (int i = 1; i <= 10000; i++)
+                Thread.Sleep(5000);
+        }
+
+        static void Main(string[] args)
+        {
+            Thread new_th = new Thread(Count);
+
+            new_th.Name = "Второй поток";
+            Console.WriteLine($"Статус {new_th.Name}: {new_th.ThreadState}");
+            new_th.Start();
+
+            for (int i = 0; i < 5; i++)
+                Console.WriteLine($"Статус {new_th.Name}: {new_th.ThreadState}");
+
+
+        }
+    }
+}
