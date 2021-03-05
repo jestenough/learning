28aa176a9607db605fe9e7cad0d54980d53f836d
Vyacheslav Dmitriev <rtefforg@gmail.com>
2021-03-05 09:03:25 +0300

    Update: C# folder [Languages]
    Add: Lab-24

---
+++ Languages/C#/Labs/Lab-24/Task-2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace Task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Parallel.For(1, 10, Factorial);
+            ParallelLoopResult result = Parallel.ForEach<int>(new List<int>() { 1, 3, 5, 8 },Factorial);
+
+        }
+
+        static void Factorial(int x)
+        {
+            int result = 1;
+            for (int i = 1; i <= x; i++)
+                result *= i;
+
+            Console.WriteLine("Выполняется задача {0}", Task.CurrentId);
+            Console.WriteLine("Факториал числа {0} равен {1}", x, result);
+            Thread.Sleep(3000);
+        }
+
+    }
+}
---
+++ Languages/C#/Labs/Lab-24/Task-3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace Task_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            ParallelLoopResult result = Parallel.ForEach<int>(new List<int>() {rnd.Next(1, 50), rnd.Next(1,50), rnd.Next(1,50)}, prime_number);
+        }
+
+        static void prime_number(int n)
+        {
+            bool prost = true;
+
+            for (int i = 2; i <= n / 2; i++)
+            {
+                if (n % i == 0)
+                {
+                    prost = false;
+                    break;
+                }
+            }
+            if (prost)
+            {
+                Console.WriteLine($"Число {n} простое");
+            }
+            else
+            {
+                Console.WriteLine($"Число {n} НЕ ПРОСТОЕ");
+            }
+        }
+    }
+}
